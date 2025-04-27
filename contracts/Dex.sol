91b01fbd5d6a2dfb2f47dda59e8594b8bf4499ae
mamatt <mamatttala@gmail.com>
2025-04-27 18:44:20 +0200

    Create Dex.sol

---
+++ contracts/Dex.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract Dex {
+    address payable public owner;
+
+    IERC20 private dai;
+    IERC20 private usdc;
+
+    // exchange rate indexes
+    uint256 dexARate = 90;
+    uint256 dexBRate = 100;
+
+    // keeps track of individuals' dai balances
+    mapping(address => uint256) public daiBalances;
+
+    // keeps track of individuals' USDC balances
+    mapping(address => uint256) public usdcBalances;
+
+    constructor(address  _daiAddress, address _usdcAddress) {
+        owner = payable(msg.sender);
+        dai = IERC20(_daiAddress);
+        usdc = IERC20(_usdcAddress);
+    }
+
+    function depositUSDC(uint256 _amount) external {
+        usdcBalances[msg.sender] += _amount;
+        uint256 allowance = usdc.allowance(msg.sender, address(this));
+        require(allowance >= _amount, "Check the token allowance");
+        usdc.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function depositDAI(uint256 _amount) external {
+        daiBalances[msg.sender] += _amount;
+        uint256 allowance = dai.allowance(msg.sender, address(this));
+        require(allowance >= _amount, "Check the token allowance");
+        dai.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function buyDAI() external {
+        uint256 daiToReceive = ((usdcBalances[msg.sender] / dexARate) * 100) *
+            (10**12);
